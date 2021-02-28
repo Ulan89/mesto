6cdd2593398232603ea025276b26c6fd086e389b
Ulan Pazylov <pazyloffu@yandex.ru>
2021-02-28 05:15:24 +0300

    commit 4

--- script/script.js
+++ script/script.js
@@ -2,13 +2,19 @@ let popupOpen = document.querySelector('.popup');
 let popupClose = document.querySelector('.popup__close');
 let editButton = document.querySelector('.profile__edit-button');
 
-editButton.addEventListener('click', function(){
-    popupOpen.classList.add('popup_active')
-});
+function btnClick(evt) {
+    popupOpen.classList.add('popup_active');
+    popupName.value = profileName.textContent;
+    popupJob.value = profileJob.textContent; 
+}
+editButton.addEventListener('click', btnClick);
 
-popupClose.addEventListener('click', function(){
+
+function btnRm() {
     popupOpen.classList.remove('popup_active');
-})
+}    
+
+popupClose.addEventListener('click', btnRm);
 
 
 let popupName = document.querySelector('#popup__name');
@@ -16,26 +22,22 @@ let popupJob = document.querySelector('#popup__job');
 let popupSubmit = document.querySelector('.popup__submit-button');
 let profileName = document.querySelector('.profile__title');
 let profileJob = document.querySelector('.profile__job');
-popupName.value += profileName.textContent;
-popupJob.value += profileJob.textContent;
 
-popupSubmit.addEventListener('click', function(evt){
+function btnSub(evt) {
     evt.preventDefault();
     profileName.textContent = popupName.value;
     profileJob.textContent = popupJob.value;
+
+    btnRm();
+}    
+
+popupSubmit.addEventListener('click', btnSub);
+   
     
     
     
-})
 
-let likeBtn = document.querySelectorAll('.card__button');
 
- for (let i of likeBtn){
-     i.addEventListener('click', function(){
-         i.classList.toggle('card__button_active');
-
-     })
- }
 
 
 
